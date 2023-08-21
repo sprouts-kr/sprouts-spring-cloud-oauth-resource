c1469a68aa111604f28fd238c1421157afa2fc03
Jinwon Seo <jwseo@live.co.kr>
2023-08-21 14:37:08 +0900

    Add password encoder.

--- src/main/java/kr/sprouts/framework/oauth/resource/ResourceApplication.java
+++ src/main/java/kr/sprouts/framework/oauth/resource/ResourceApplication.java
@@ -2,9 +2,12 @@ package kr.sprouts.framework.oauth.resource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.annotation.PostConstruct;
+import java.security.SecureRandom;
 import java.time.ZoneId;
 import java.util.TimeZone;
 
@@ -18,5 +21,10 @@ public class ResourceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ResourceApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10, new SecureRandom());
     }
 }
