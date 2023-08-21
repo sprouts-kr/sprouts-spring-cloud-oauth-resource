a9c3dee2dbd95bd7a90a931ece6f54298a2cd91f
Jinwon Seo <jwseo@live.co.kr>
2023-08-21 17:16:36 +0900

    Add uuid patternMatcher.

---
+++ src/main/java/kr/sprouts/framework/oauth/resource/components/utility/PatternMatcher.java
@@ -0,0 +1,11 @@
+package kr.sprouts.framework.oauth.resource.components.utility;
+
+import java.util.regex.Pattern;
+
+public class PatternMatcher {
+    private static final String UUID_REGEX = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$";
+
+    public static Boolean Uuid(String source) {
+        return Pattern.compile(UUID_REGEX).matcher(source).find();
+    }
+}
