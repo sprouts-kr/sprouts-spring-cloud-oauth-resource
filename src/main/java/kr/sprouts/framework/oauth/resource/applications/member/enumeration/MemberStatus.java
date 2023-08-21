0229e4dd18c1cf625e9a71a0635dfc03bb1140c5
Jinwon Seo <jwseo@live.co.kr>
2023-08-21 14:35:19 +0900

    Add member entity, status enum.

---
+++ src/main/java/kr/sprouts/framework/oauth/resource/applications/member/enumeration/MemberStatus.java
@@ -0,0 +1,9 @@
+package kr.sprouts.framework.oauth.resource.applications.member.enumeration;
+
+public enum MemberStatus {
+    PENDING_APPROVAL,
+    ACTIVE,
+    DEACTIVATED,
+    BLOCKED,
+    ;
+}
